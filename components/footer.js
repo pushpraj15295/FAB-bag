378b143aaf7526d1a4e72bb261c81fcc656b89df
pushpraj15295 <pushpraj15295@gmail.com>
2022-05-05 20:20:12 +0530

    adding footer part

---
+++ components/footer.js
@@ -0,0 +1,61 @@
+function footerdata(){
+
+    return ` <div id="foot_div">
+             
+    <div class="container">
+        <div class="footer-col">
+          <h4>INFORMATION</h4>
+          <br>
+          <ul>
+               <li><a href="about.html">About Us</a></li>
+               <li><a href="brand.html">Associate Brands</a></li>
+               <li><a href="FAQ.html">FAQ</a></li>
+               <li><a href="collaborate.html">Collaborative with Fab Bag</a></li>
+               <li><a href="contact.html">Contact Us</a></li>
+               <li><a href="return.html">Returns & Refunds</a></li>
+               <li><a href="term.html">Terms & Conditions</a></li>
+               <li><a href="privacy.html">Privecy Policy</a></li>
+              
+          </ul>
+
+        </div>
+
+        <div class="footer-col">
+          <h4>CUSTOMER CARE</h4>
+          <br>
+           <p>Call: <a class='cont' href="foot.html"> 022-6805-6010 </a> </p>
+           <p>(Monday to Friday : 9 AM - 7 PM).</p>
+           <p>Mail : <a class='cont' href="#"> crew@fabbag.com </a> </p>
+        </div>
+
+        <div class="footer-col">
+          <h4>NEWSLETTER</h4>
+          <br>
+          <p>Signup for the latest offers, updates and news</p>
+          <input type="text" placeholder="your email Address">
+           <button id="button">Subscribe</button>
+           <br><br>
+           <p>Don’t worry we don’t spam</p>
+        </div>
+
+    </div>
+    
+ 
+ <div id="icon">
+        <a id="f" href="https://www.facebook.com/fabbag"><i class="fab fa-facebook-f"></i></a>
+        <a id="t" href="https://twitter.com/thefabbag"><i class="fab fa-twitter"></i></a>
+        <a id="i" href="https://www.instagram.com/thefabbag/"><i class="fab fa-instagram"></i></a>
+        <a id="y" href="https://www.youtube.com/fabbag"><i class="fab fa-youtube"></i></a>
+         <br><br>
+        <p>Copyright © 2021 fabbag.com All rights reserved.</p>
+ </div>
+
+</div>  `
+
+
+}
+
+
+
+export default footerdata
+
